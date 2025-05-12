5014958c3637982fc5fc711b3b71598357ceebc7
sagarp96 <sagarcool2345@gmail.com>
2025-05-12 19:51:59 +0530

    Javascript-Logic

---
+++ script.js
@@ -0,0 +1,29 @@
+function startGame() {
+  const playerOne = 0;
+  const playertwo = 1;
+
+  function createBoard() {}
+  const arra1 = [0, 0, 0];
+  const arra2 = [0, 0, 0];
+  const arra3 = [0, 0, 0];
+
+  function getUserInput() {}
+}
+// player selects the area
+
+// player1 selects the arra1[1]
+function playerSelects() {}
+//player2 selecter arr2[1]
+
+buttonstart.startGame();
+buttonrestart.restartGame();
+
+//player2 selector
+
+function resetGame() {}
+
+function switchPlayer(playerOne) {
+  playerOne = 1;
+}
+
+//This is the main game function
